f12ce4603ecc876523f001e812a85ea3ce424544
Christoph Wurst <wurstchristoph@users.noreply.github.com>
2014-05-03 16:06:23 +0200

    abstract tastk added

---
+++ LTF4/Tasks/Task.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using Robot;
+
+namespace Robot
+{
+	public abstract class Task
+	{
+		internal Boolean running = false;
+		internal Thread proc = null;
+
+		internal virtual void Loop() {}
+		public virtual void Init () {}
+
+		public void Start() {
+			Log.Debug ("process starting");
+			running = true;
+			proc.Start ();
+			Log.Debug ("process started");
+		}
+
+		public void Stop() {
+			Log.Debug ("process stopping");
+			running = false;
+			//wait for threat to finish
+			proc.Join ();
+			Log.Debug ("process stopped");
+		}
+	}
+}
+
