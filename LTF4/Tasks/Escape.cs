efe5d3036b219c769cf503134a9cc3da80e124df
kathi <unknown-user@10.0.0.8>
2014-05-05 21:17:10 +0200

    rand turn

--- LTF4/Tasks/Escape.cs
+++ LTF4/Tasks/Escape.cs
@@ -10,18 +10,66 @@ namespace Robot
 		private Movement move;
 		private TouchSensor touch;
 		private IRSensor dist;
+		private Random direction_generator;   //for random turn
 	
 		internal override void Loop() {
+
 			move.Forward (50);
+
 			while (running) {
-				if ((this.touch.IsPressed ()) || (this.dist.Read () < 20)) {
+				if (this.touch.IsPressed ()) {
 					//move back a little bit
 					this.move.Backward (90, 700);
-					//turn
-					//TODO: random turn
-					this.move.TurnLeft (180);
+
+					//generate number between 0 and 1
+					double next_direction = direction_generator.Next (0,1);
+
+					//generate number between 90 and 180 for random degree 
+					int random_degree = direction_generator.Next (90, 180);
+
+					//random turn
+					if (next_direction > 0.5) {
+
+						this.move.TurnLeft (random_degree);
+
+					} else {
+
+						this.move.TurnRight (random_degree);
+					}
+
 					//move on
 					this.move.Forward (50);
+
+				}
+
+				//create random number for random turns while moving forward
+				int random_turn = direction_generator.Next (0,100);
+
+				if(random_turn == 60){
+
+					this.move.TurnLeft ();
+
+				}else if (random_turn == 30){
+
+					this.move.TurnRight ();
+				}
+
+
+
+				if (this.dist.Read () < 5) {
+					//found something
+					this.move.Brake ();
+					switch (this.color.ReadColor ()) {
+					case Color.Blue:
+						Log.Info ("found something blue");
+						break;
+					case Color.Red:
+						Log.Info ("found something red");
+						break;
+					default:
+						Log.Info ("unknown color found");
+						break;
+					}
 				}
 				Thread.Sleep (0);
 			}
